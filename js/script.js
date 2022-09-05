46cac585b3fe6e6550472f4ee8301f7cfc73ea31
Brayden Wilder <braydenwilder@gmail.com>
2022-09-05 17:27:02 -0600

    index.html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// X Make an interface that contains the numbers and operators
+// X Make a clear button a back button and an equal button
+// X Make a display that shows the math
+// Make functions to do the math
+// Make a function that calls on the math functions when appropriate 
+// Make a way to store the values that are being clicked
+// EXTRA CREDIT
+// Make a way to add only one decimal point per number
+// Round the decimals a few places
+
+const numbers = document.querySelectorAll('button');
